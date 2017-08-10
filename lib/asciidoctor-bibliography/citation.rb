6cdce709d820fd2652778f64c483a15d929f53a8
Paolo Brasolin <paolo@leanbit.it>
2017-08-10 08:24:28 +0200

    iplement new syntax

--- lib/asciidoctor-bibliography/citation.rb
+++ lib/asciidoctor-bibliography/citation.rb
@@ -2,20 +2,36 @@ require 'asciidoctor/attribute_list'
 
 module AsciidoctorBibliography
   class Citation
-    REGEXP = /\\?(cite):(\S+)?\[(|.*?[^\\])\]/
+    REGEXP = /\\?(cite):(?:(\S*?)?\[(|.*?[^\\])\])(?:\+(\S*?)?\[(|.*?[^\\])\])*/
 
-    attr_reader :macro, :target, :attributes, :keys
+    attr_reader :macro, :cites
 
-    def initialize(macro, target, attributes)
+    def initialize(macro, *targets_and_attributes_list)
       @macro = macro
-      @target = target
-      @attributes = ::Asciidoctor::AttributeList.new(attributes).parse
-      # Bibliographic keys are all and only the positional attributes.
-      @keys = @attributes.select { |hash_key, _| hash_key.is_a? Integer }.values
+      @cites = []
+      targets_and_attributes_list.compact.each_slice(2).each do |target, attributes|
+        positional_attributes, named_attributes = # true, false
+          ::Asciidoctor::AttributeList.new(attributes).parse
+            .group_by { |hash_key, _| hash_key.is_a? Integer }
+            .values.map { |a| Hash[a] }
+        positional_attributes = positional_attributes.values
+        @cites << {
+          target: target,
+          key: positional_attributes.first,
+          attributes: {
+            positional: positional_attributes,
+            named: named_attributes
+          }
+        }
+      end
     end
 
     def render(formatter)
-      @keys.map { |key| render_key(formatter, key) }.join(', ')
+      keys.map { |key| render_xref(formatter, key) }.join(', ')
+    end
+
+    def keys
+      @cites.map { |h| h[:key] }
     end
 
     private
@@ -28,7 +44,7 @@ module AsciidoctorBibliography
       formatter.render(:citation, id: key)
     end
 
-    def render_key(formatter, key)
+    def render_xref(formatter, key)
       "xref:#{render_id(key)}[#{render_label(formatter, key)}]"
     end
   end
