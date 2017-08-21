887704464c3be7196f9fd74910950f0b1534e514
Paolo Brasolin <paolo@leanbit.it>
2017-08-21 23:38:43 +0200

    bibliography-order (appearance/alphabetical) implemented

--- lib/asciidoctor-bibliography/index.rb
+++ lib/asciidoctor-bibliography/index.rb
@@ -15,13 +15,23 @@ module AsciidoctorBibliography
     end
 
     def render(bibliographer)
-      lines = []
+      unsorted_lines = []
       bibliographer.occurring_keys.each_with_index do |target, index|
         line = '{empty}'
         line << "[#{index + 1}] " if bibliographer.options['citation-style'] == 'numbers'
         line << render_entry(target, bibliographer.index_formatter)
-        lines << line
+        if bibliographer.options['order'] == 'alphabetical'
+          sortable_index = first_author_family_name(target, bibliographer)
+        elsif bibliographer.options['order'] == 'appearance'
+          sortable_index = index
+        else # defaults to appearance
+          sortable_index = index
+        end
+
+        unsorted_lines << { line: line, sortable_index: sortable_index }
       end
+
+      lines = unsorted_lines.sort_by { |l| l[:sortable_index] }.map { |l| l[:line] }
 
       # Intersperse the lines with empty ones to render as paragraphs.
       lines.join("\n\n").lines.map(&:strip)
@@ -37,6 +47,14 @@ module AsciidoctorBibliography
 
     def render_entry(target, formatter)
       "anchor:#{render_entry_id(target)}[]#{render_entry_label(target, formatter)}"
+    end
+
+    private
+
+    def first_author_family_name(key, bibliographer)
+      authors = bibliographer.database.find{ |h| h['id'] == key }['author']
+      return "" if authors.nil?
+      authors.map{ |h| h['family'] }.compact.first # TODO: is the first also alphabetically the first?
     end
   end
 end
