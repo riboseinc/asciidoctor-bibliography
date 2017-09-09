0c4ce7142fbf27edd3431ad6a5d7aac2129fcc40
Paolo Brasolin <paolo@leanbit.it>
2017-09-10 01:16:51 +0200

    fix citation-labels for indices

--- lib/asciidoctor-bibliography/citation.rb
+++ lib/asciidoctor-bibliography/citation.rb
@@ -15,10 +15,10 @@ module AsciidoctorBibliography
       @uuid = SecureRandom.uuid
       @macro = macro
       @citation_items = []
-      target_and_attributes_list_pairs.compact.each_slice(2).each do |target, attribute_list|
+      target_and_attributes_list_pairs.compact.each_slice(2).each do |_target, attribute_list|
         @citation_items << CitationItem.new do |cite|
           # NOTE: we're not doing anything with targets right now.
-          # cite.target = target
+          # cite.target = _target
           cite.parse_attribute_list attribute_list
         end
       end
