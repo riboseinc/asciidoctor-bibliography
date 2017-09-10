ea4a46bc5d838a3491fe9e9fdd233ba30c23f7bf
Paolo Brasolin <paolo.brasolin@gmail.com>
2017-09-10 20:26:35 +0200

    write spec for db

---
+++ spec/database.rb
@@ -0,0 +1,39 @@
+require 'asciidoctor-bibliography/helpers'
+
+describe AsciidoctorBibliography::Database do
+  describe '.new' do
+    it 'is by default an empty array' do
+      expect(described_class.new).to eq([])
+    end
+
+    it 'can be initialized with a single database' do
+      expect(described_class.new("spec/fixtures/database.bib")).to_not eq([])
+    end
+
+    it 'can be initialized with a list of databases' do
+      expect(described_class.new("spec/fixtures/database.bib", "spec/fixtures/database.bibtex")).to_not eq([])
+    end
+  end
+
+  describe '#append' do
+    let(:db) { described_class.new }
+
+    it 'can load and concatenate databases after initialization' do
+      expect(db.length).to eq(0)
+      expect { db.append("spec/fixtures/database.bib") }.to change { db.length }
+      expect { db.append("spec/fixtures/database.bibtex") }.to change { db.length }
+    end
+  end
+
+  describe '.load' do
+    it 'raises error if given unknown format' do
+      expect { described_class.load 'spec/fixtures/database.unk' }
+        .to raise_exception AsciidoctorBibliography::Exceptions::DatabaseFormatNotSupported
+    end
+
+    it 'recognizes Bib(La)Tex databases' do
+      expect { described_class.load 'spec/fixtures/database.bib' }.to_not raise_exception
+      expect { described_class.load 'spec/fixtures/database.bibtex' }.to_not raise_exception
+    end
+  end
+end
