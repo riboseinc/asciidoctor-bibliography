203685917a47d948e599f3eae66ac2abc83ddda7
Paolo Brasolin <paolo.brasolin@gmail.com>
2017-09-10 21:34:36 +0200

    add citation_item spec

--- lib/asciidoctor-bibliography/citation_item.rb
+++ lib/asciidoctor-bibliography/citation_item.rb
@@ -11,17 +11,17 @@ module AsciidoctorBibliography
     def locators
       Helpers
         .slice(named_attributes || {}, *CiteProc::CitationItem.labels.map(&:to_s))
-        .reject! { |_, value| value.nil? || value.empty? } # equivalent to Hash#compact
+        .reject { |_, value| value.nil? || value.empty? } # equivalent to Hash#compact
     end
 
-    def parse_attribute_list(attributes)
+    def parse_attribute_list(string)
       parsed_attributes =
-        ::Asciidoctor::AttributeList.new(attributes).parse
+        ::Asciidoctor::AttributeList.new(string).parse
                                     .group_by { |hash_key, _| hash_key.is_a? Integer }
                                     .values.map { |a| Hash[a] }
       self.positional_attributes = parsed_attributes.first.values
       self.named_attributes = parsed_attributes.last
-      self.key = positional_attributes.first
+      self.key = positional_attributes.shift
     end
   end
 end
