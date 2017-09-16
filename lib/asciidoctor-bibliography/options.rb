964dc04d16e527325a77f3023321f862c22a2154
Paolo Brasolin <paolo.brasolin@gmail.com>
2017-09-16 18:02:04 +0200

    Options cleanup

--- lib/asciidoctor-bibliography/options.rb
+++ lib/asciidoctor-bibliography/options.rb
@@ -4,16 +4,29 @@ require 'csl/styles'
 
 module AsciidoctorBibliography
   class Options < Hash
-    def validate!
+    PREFIX = 'bibliography-'.freeze
+
+    DEFAULTS = {
+      'bibliography-database' => nil,
+      'bibliography-style' => 'apa',
+      'bibliography-hyperlinks' => 'true',
+      'bibliography-tex-mode' => 'false', # TODO: implement
+      'bibliography-order' => 'alphabetical', # TODO: deprecate
+      'bibliography-citation-style' => 'authoryear', # TODO: deprecate
+      'bibliography-sort' => nil
+    }.freeze
+
+    def initialize
+      merge DEFAULTS
     end
 
     def style
-      # Error throwing delegated to CSL library.
-      self['style']
+      # Error throwing delegated to CSL library. Seems to have nice messages.
+      self['bibliography-style'] || DEFAULTS['bibliography-style']
     end
 
     def hyperlinks?
-      value = self['hyperlinks']
+      value = self['bibliography-hyperlinks'] || DEFAULTS['bibliography-hyperlinks']
       unless %w[true false].include? value
         message = "Option :bibliography-hyperlinks: has an invalid value (#{value}). Allowed values are 'true' and 'false'."
         raise Errors::Options::Invalid, message
@@ -23,7 +36,7 @@ module AsciidoctorBibliography
     end
 
     def database
-      value = self['database']
+      value = self['bibliography-database'] || DEFAULTS['bibliography-database']
       if value.nil?
         message = 'Option :bibliography-database: is mandatory. A bibliographic database is required.'
         raise Errors::Options::Missing, message
@@ -45,27 +58,26 @@ module AsciidoctorBibliography
       value
     end
 
-    PREFIX = 'bibliography-'.freeze
-
-    DEFAULTS = {
-      'order' => 'alphabetical',
-      'style' => 'apa',
-      'citation-style' => 'authoryear',
-      'hyperlinks' => 'true',
-      'database' => nil,
-      'bibliography-sort' => nil
-    }.freeze
-
-    def self.new_from_reader(reader)
-      new.merge get_options_hash(reader)
+    def self.validate_parsed_sort_type!(value)
+      return value if value.nil?
+      return value if value.is_a?(Array) && value.all? { |v| v.is_a? Hash }
+      return [value] if value.is_a? Hash
+      message = "Option :bibliography-sort: has an invalid value (#{value}). Please refer to manual for more info."
+      raise Errors::Options::Invalid, message
     end
 
-    def self.get_options_hash(reader)
+    def self.validate_parsed_sort_contents!(array)
+      # TODO: should we restrict these? Double check the CSL spec.
+      allowed_keys = %w[variable macro sort names-min names-use-first names-use-last]
+      return array unless array.any? { |hash| (hash.keys - allowed_keys).any? }
+      message = "Option :bibliography-sort: has a value containing invalid keys (#{array}). Allowed keys are #{allowed_keys.inspect}. Please refer to manual for more info."
+      raise Errors::Options::Invalid, message
+    end
+
+    def self.new_from_reader(reader)
       header_attributes = get_header_attributes_hash reader
-      bibliography_options = filter_attributes header_attributes
-      DEFAULTS
-        .merge(bibliography_options)
-        .reject { |_, value| value.nil? || value.empty? }.to_h
+      header_attributes.select! { |key, _| DEFAULTS.keys.include? key }
+      new.merge header_attributes
     end
 
     def self.get_header_attributes_hash(reader)
@@ -77,29 +89,6 @@ module AsciidoctorBibliography
       ::Asciidoctor::Parser
         .parse(tmp_reader, tmp_document, header_only: true)
         .attributes
-    end
-
-    def self.filter_attributes(hash)
-      Helpers
-        .slice(hash, *DEFAULTS.keys.map { |k| "#{PREFIX}#{k}" })
-        .map { |k, v| [k[PREFIX.length..-1], v] }.to_h
-        .reject { |_, value| value.nil? || value.empty? }.to_h
-      # NOTE: at this point valid values are nonempty strings.
-    end
-
-    def self.validate_parsed_sort_type!(value)
-      return value if value.nil?
-      return value if value.is_a?(Array) && value.all { |v| v.is_a? Hash }
-      return [value] if value.is_a? Hash
-      message = "Option :bibliography-sort: has an invalid value (#{value}). Please refer to manual for more info."
-      raise Errors::Options::Invalid, message
-    end
-
-    def self.validate_parsed_sort_contents!(array)
-      allowed_keys = %w[variable macro sort names-min names-use-first names-use-last]
-      return array unless array.any? { |hash| (hash.keys - allowed_keys).any? }
-      message = "Option :bibliography-sort: has a value containing invalid keys (#{array}). Allowed keys are #{allowed_keys.inspect}. Please refer to manual for more info."
-      raise Errors::Options::Invalid, message
     end
   end
 end
