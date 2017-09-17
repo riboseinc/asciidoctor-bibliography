a6d3efd426c87f17838f2b82e49553158de7da40
Paolo Brasolin <paolo.brasolin@gmail.com>
2017-09-17 16:25:47 +0200

    cleanup specs

--- spec/database_spec.rb
+++ spec/database_spec.rb
@@ -1,44 +1,44 @@
-require 'asciidoctor-bibliography/helpers'
+require "asciidoctor-bibliography/helpers"
 
 describe AsciidoctorBibliography::Database do
-  describe '.new' do
-    it 'is by default an empty array' do
+  describe ".new" do
+    it "is by default an empty array" do
       expect(described_class.new).to eq([])
     end
 
-    it 'can be initialized with a single database' do
+    it "can be initialized with a single database" do
       expect(described_class.new("spec/fixtures/database.bib")).to_not eq([])
     end
 
-    it 'can be initialized with a list of databases' do
+    it "can be initialized with a list of databases" do
       expect(described_class.new("spec/fixtures/database.bib", "spec/fixtures/database.bibtex")).to_not eq([])
     end
   end
 
-  describe '#append' do
+  describe "#append" do
     let(:db) { described_class.new }
 
-    it 'can load and concatenate databases after initialization' do
+    it "can load and concatenate databases after initialization" do
       expect(db.length).to eq(0)
-      expect { db.append("spec/fixtures/database.bib") }.to change { db.length }
-      expect { db.append("spec/fixtures/database.bibtex") }.to change { db.length }
+      expect { db.append("spec/fixtures/database.bib") }.to(change { db.length })
+      expect { db.append("spec/fixtures/database.bibtex") }.to(change { db.length })
     end
   end
 
-  describe '.load' do
-    it 'raises error if given non existing file' do
-      expect { described_class.load 'spec/fixtures/database.xxx' }
-        .to raise_exception AsciidoctorBibliography::Errors::Database::FileNotFound
+  describe ".load" do
+    it "raises error if given non existing file" do
+      expect { described_class.load "spec/fixtures/database.xxx" }.
+        to raise_exception AsciidoctorBibliography::Errors::Database::FileNotFound
     end
 
-    it 'raises error if given unknown format' do
-      expect { described_class.load 'spec/fixtures/database.unk' }
-        .to raise_exception AsciidoctorBibliography::Errors::Database::UnsupportedFormat
+    it "raises error if given unknown format" do
+      expect { described_class.load "spec/fixtures/database.unk" }.
+        to raise_exception AsciidoctorBibliography::Errors::Database::UnsupportedFormat
     end
 
-    it 'recognizes Bib(La)Tex databases' do
-      expect { described_class.load 'spec/fixtures/database.bib' }.to_not raise_exception
-      expect { described_class.load 'spec/fixtures/database.bibtex' }.to_not raise_exception
+    it "recognizes Bib(La)Tex databases" do
+      expect { described_class.load "spec/fixtures/database.bib" }.to_not raise_exception
+      expect { described_class.load "spec/fixtures/database.bibtex" }.to_not raise_exception
     end
   end
 end
--- spec/options_spec.rb
+++ spec/options_spec.rb
@@ -1,43 +1,43 @@
-require 'asciidoctor-bibliography/helpers'
+require "asciidoctor-bibliography/helpers"
 
 describe AsciidoctorBibliography::Options do
-  describe '#database' do
-    it 'has no default' do
-      expect { described_class.new.database }
-        .to raise_exception AsciidoctorBibliography::Errors::Options::Missing
+  describe "#database" do
+    it "has no default" do
+      expect { described_class.new.database }.
+        to raise_exception AsciidoctorBibliography::Errors::Options::Missing
     end
 
-    it 'returns the provided database name' do
-      expect(described_class.new.merge({'bibliography-database' => 'foobar'}).database).to eq('foobar')
+    it "returns the provided database name" do
+      expect(described_class.new.merge("bibliography-database" => "foobar").database).to eq("foobar")
     end
   end
 
-  describe '#hyperlinks?' do
-    it 'defaults to true' do
+  describe "#hyperlinks?" do
+    it "defaults to true" do
       expect(described_class.new.hyperlinks?).to be true
     end
 
-    it 'returns true when provided option is true' do
-      expect(described_class.new.merge({'bibliography-hyperlinks' => 'false'}).hyperlinks?).to be false
+    it "returns true when provided option is true" do
+      expect(described_class.new.merge("bibliography-hyperlinks" => "false").hyperlinks?).to be false
     end
 
-    it 'returns true when provided option is true' do
-      expect(described_class.new.merge('bibliography-hyperlinks' => 'true').hyperlinks?).to be true
+    it "returns true when provided option is true" do
+      expect(described_class.new.merge("bibliography-hyperlinks" => "true").hyperlinks?).to be true
     end
 
-    it 'raises an error when provided option is invalid' do
-      expect { described_class.new.merge('bibliography-hyperlinks' => 'foo').hyperlinks? }
-        .to raise_exception AsciidoctorBibliography::Errors::Options::Invalid
+    it "raises an error when provided option is invalid" do
+      expect { described_class.new.merge("bibliography-hyperlinks" => "foo").hyperlinks? }.
+        to raise_exception AsciidoctorBibliography::Errors::Options::Invalid
     end
   end
 
-  describe '#style' do
-    it 'defaults to apa' do
-      expect(described_class.new.style).to eq 'apa'
+  describe "#style" do
+    it "defaults to apa" do
+      expect(described_class.new.style).to eq "apa"
     end
 
-    it 'returns the provided style name' do
-      expect(described_class.new.merge({'bibliography-style' => 'foobar'}).style).to eq('foobar')
+    it "returns the provided style name" do
+      expect(described_class.new.merge("bibliography-style" => "foobar").style).to eq("foobar")
     end
   end
 end
