4881c3286b3f4bc6a8d4957ef6cc15d3dd59e9a6
Paolo Brasolin <paolo.brasolin@gmail.com>
2017-09-23 19:12:43 +0200

    add citeal_ csl styles

--- spec/csl/styles/citet_spec.rb
+++ spec/csl/styles/citet_spec.rb
@@ -182,3 +182,167 @@ describe "citep* macro" do
       to eq "(Erdős, Heyting, and Brouwer, 1965, somewhere)"
   end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+describe "citealt macro" do
+  it "formats a single citation" do
+    expect(formatted_citation("citealt:[Erdos65]")).
+      to eq "Erdős et al. 1965"
+  end
+
+  it "formats a grouped citation" do
+    expect(formatted_citation("citealt:[Erdos65]+[Einstein35]")).
+      to eq "Erdős et al. 1965; Einstein et al. 1935"
+  end
+
+  it "formats a single citation with a prefix" do
+    expect(formatted_citation("citealt:[Erdos65, prefix=see]")).
+      to eq "Erdős et al. see 1965"
+  end
+
+  it "formats a single citation with a suffix" do
+    expect(formatted_citation("citealt:[Erdos65, suffix=new edition]")).
+      to eq "Erdős et al. 1965, new edition"
+  end
+
+  it "formats a single citation with both a prefix and a suffix" do
+    expect(formatted_citation("citealt:[Erdos65, prefix=see, suffix=new edition]")).
+      to eq "Erdős et al. see 1965, new edition"
+  end
+
+  it "formats a single citation with a standard locator" do
+    expect(formatted_citation("citealt:[Erdos65, page=41-43]")).
+      to eq "Erdős et al. 1965, pp. 41-43"
+  end
+
+  it "formats a single citation with a custom locator" do
+    expect(formatted_citation("citealt:[Erdos65, locator=somewhere]")).
+      to eq "Erdős et al. 1965, somewhere"
+  end
+end
+
+
+describe "citealt* macro" do
+  it "formats a single citation" do
+    expect(formatted_citation("citealt*:[Erdos65]")).
+      to eq "Erdős, Heyting, and Brouwer 1965"
+  end
+
+  it "formats a grouped citation" do
+    expect(formatted_citation("citealt*:[Erdos65]+[Einstein35]")).
+      to eq "Erdős, Heyting, and Brouwer 1965; Einstein, Podolsky, and Rosen 1935"
+  end
+
+  it "formats a single citation with a prefix" do
+    expect(formatted_citation("citealt*:[Erdos65, prefix=see]")).
+      to eq "Erdős, Heyting, and Brouwer see 1965"
+  end
+
+  it "formats a single citation with a suffix" do
+    expect(formatted_citation("citealt*:[Erdos65, suffix=new edition]")).
+      to eq "Erdős, Heyting, and Brouwer 1965, new edition"
+  end
+
+  it "formats a single citation with both a prefix and a suffix" do
+    expect(formatted_citation("citealt*:[Erdos65, prefix=see, suffix=new edition]")).
+      to eq "Erdős, Heyting, and Brouwer see 1965, new edition"
+  end
+
+  it "formats a single citation with a standard locator" do
+    expect(formatted_citation("citealt*:[Erdos65, page=41-43]")).
+      to eq "Erdős, Heyting, and Brouwer 1965, pp. 41-43"
+  end
+
+  it "formats a single citation with a custom locator" do
+    expect(formatted_citation("citealt*:[Erdos65, locator=somewhere]")).
+      to eq "Erdős, Heyting, and Brouwer 1965, somewhere"
+  end
+end
+
+describe "citealp macro" do
+  it "formats a single citation" do
+    expect(formatted_citation("citealp:[Erdos65]")).
+      to eq "Erdős et al., 1965"
+  end
+
+  it "formats a grouped citation" do
+    expect(formatted_citation("citealp:[Erdos65]+[Einstein35]")).
+      to eq "Erdős et al., 1965; Einstein et al., 1935"
+  end
+
+  it "formats a single citation with a prefix" do
+    expect(formatted_citation("citealp:[Erdos65, prefix=see]")).
+      to eq "see Erdős et al., 1965"
+  end
+
+  it "formats a single citation with a suffix" do
+    expect(formatted_citation("citealp:[Erdos65, suffix=new edition]")).
+      to eq "Erdős et al., 1965, new edition"
+  end
+
+  it "formats a single citation with both a prefix and a suffix" do
+    expect(formatted_citation("citealp:[Erdos65, prefix=see, suffix=new edition]")).
+      to eq "see Erdős et al., 1965, new edition"
+  end
+
+  it "formats a single citation with a standard locator" do
+    expect(formatted_citation("citealp:[Erdos65, page=41-43]")).
+      to eq "Erdős et al., 1965, pp. 41-43"
+  end
+
+  it "formats a single citation with a custom locator" do
+    expect(formatted_citation("citealp:[Erdos65, locator=somewhere]")).
+      to eq "Erdős et al., 1965, somewhere"
+  end
+end
+
+describe "citealp* macro" do
+  it "formats a single citation" do
+    expect(formatted_citation("citealp*:[Erdos65]")).
+      to eq "Erdős, Heyting, and Brouwer, 1965"
+  end
+
+  it "formats a grouped citation" do
+    expect(formatted_citation("citealp*:[Erdos65]+[Einstein35]")).
+      to eq "Erdős, Heyting, and Brouwer, 1965; Einstein, Podolsky, and Rosen, 1935"
+  end
+
+  it "formats a single citation with a prefix" do
+    expect(formatted_citation("citealp*:[Erdos65, prefix=see]")).
+      to eq "see Erdős, Heyting, and Brouwer, 1965"
+  end
+
+  it "formats a single citation with a suffix" do
+    expect(formatted_citation("citealp*:[Erdos65, suffix=new edition]")).
+      to eq "Erdős, Heyting, and Brouwer, 1965, new edition"
+  end
+
+  it "formats a single citation with both a prefix and a suffix" do
+    expect(formatted_citation("citealp*:[Erdos65, prefix=see, suffix=new edition]")).
+      to eq "see Erdős, Heyting, and Brouwer, 1965, new edition"
+  end
+
+  it "formats a single citation with a standard locator" do
+    expect(formatted_citation("citealp*:[Erdos65, page=41-43]")).
+      to eq "Erdős, Heyting, and Brouwer, 1965, pp. 41-43"
+  end
+
+  it "formats a single citation with a custom locator" do
+    expect(formatted_citation("citealp*:[Erdos65, locator=somewhere]")).
+      to eq "Erdős, Heyting, and Brouwer, 1965, somewhere"
+  end
+end
+
+
