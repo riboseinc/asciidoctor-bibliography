a90e78bcc53af17ba6c6a56279d09df42ca76d48
Paolo Brasolin <paolo.brasolin@gmail.com>
2017-09-24 13:17:59 +0200

    spec/style cleanup; implement locale

---
+++ spec/csl/styles/citets_spec.rb
@@ -0,0 +1,40 @@
+# coding: utf-8
+
+require_relative "styles_helper"
+
+describe "citet* macro" do
+  it "formats a single citation" do
+    expect(formatted_citation("citet*:[Erdos65]")).
+      to eq "Erdős, Heyting, and Brouwer (1965)"
+  end
+
+  it "formats a grouped citation" do
+    expect(formatted_citation("citet*:[Erdos65]+[Einstein35]")).
+      to eq "Erdős, Heyting, and Brouwer (1965); Einstein, Podolsky, and Rosen (1935)"
+  end
+
+  it "formats a single citation with a prefix" do
+    expect(formatted_citation("citet*:[Erdos65, prefix=see]")).
+      to eq "Erdős, Heyting, and Brouwer (see 1965)"
+  end
+
+  it "formats a single citation with a suffix" do
+    expect(formatted_citation("citet*:[Erdos65, suffix=new edition]")).
+      to eq "Erdős, Heyting, and Brouwer (1965, new edition)"
+  end
+
+  it "formats a single citation with both a prefix and a suffix" do
+    expect(formatted_citation("citet*:[Erdos65, prefix=see, suffix=new edition]")).
+      to eq "Erdős, Heyting, and Brouwer (see 1965, new edition)"
+  end
+
+  it "formats a single citation with a standard locator" do
+    expect(formatted_citation("citet*:[Erdos65, page=41-43]")).
+      to eq "Erdős, Heyting, and Brouwer (1965, pp. 41-43)"
+  end
+
+  it "formats a single citation with a custom locator" do
+    expect(formatted_citation("citet*:[Erdos65, locator=somewhere]")).
+      to eq "Erdős, Heyting, and Brouwer (1965, somewhere)"
+  end
+end
---
+++ spec/csl/styles/styles_helper.rb
@@ -0,0 +1,35 @@
+# coding: utf-8
+
+require "asciidoctor-bibliography"
+
+def formatted_citation(macro, style: "ieee")
+  bibliographer = AsciidoctorBibliography::Bibliographer.new
+  bibliographer.options = AsciidoctorBibliography::Options.new.
+    merge("bibliography-hyperlinks" => "false")
+  bibliographer.database = AsciidoctorBibliography::Database.new.concat ::BibTeX.parse(<<-BIBTEX).to_citeproc
+    @article{Erdos65,
+      title = {Some very hard sums},
+      journal={Difficult Maths Today},
+      author={Paul Erdős and Arend Heyting and Luitzen Egbertus Brouwer},
+      year={1965},
+      pages={30}
+    }
+
+    @article{Einstein35,
+      title={Can quantum-mechanical description of physical reality be considered complete?},
+      author={Einstein, Albert and Podolsky, Boris and Rosen, Nathan},
+      journal={Physical review},
+      volume={47},
+      number={10},
+      pages={777},
+      year={1935},
+      publisher={APS}
+    }
+  BIBTEX
+
+  macro.gsub(AsciidoctorBibliography::Citation::REGEXP) do
+    citation = AsciidoctorBibliography::Citation.new(*Regexp.last_match.captures)
+    bibliographer.add_citation(citation)
+    citation.render bibliographer
+  end.gsub(/^{empty}/, "")
+end
