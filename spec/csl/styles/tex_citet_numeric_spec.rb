97bf14862534a0a585ffc417823ced45237efa71
Paolo Brasolin <paolo.brasolin@gmail.com>
2019-07-06 16:20:36 +0200

    Escape left square brackets on rendered citations

--- spec/csl/styles/tex_citet_numeric_spec.rb
+++ spec/csl/styles/tex_citet_numeric_spec.rb
@@ -7,36 +7,36 @@ describe "citet macro with numeric style" do
 
   it "formats a single citation" do
     expect(formatted_citation("citet:[Erdos65]", options: options)).
-      to eq "Erdős et al. [1&rsqb;"
+      to eq "Erdős et al. &lsqb;1&rsqb;"
   end
 
   it "formats a grouped citation" do
     expect(formatted_citation("citet:[Erdos65]+[Einstein35]", options: options)).
-      to eq "Erdős et al. [1&rsqb;, Einstein et al. [2&rsqb;"
+      to eq "Erdős et al. &lsqb;1&rsqb;, Einstein et al. &lsqb;2&rsqb;"
   end
 
   it "formats a single citation with a prefix" do
     expect(formatted_citation("citet:[Erdos65, prefix=see]", options: options)).
-      to eq "Erdős et al. [see 1&rsqb;"
+      to eq "Erdős et al. &lsqb;see 1&rsqb;"
   end
 
   it "formats a single citation with a suffix" do
     expect(formatted_citation("citet:[Erdos65, suffix=new edition]", options: options)).
-      to eq "Erdős et al. [1, new edition&rsqb;"
+      to eq "Erdős et al. &lsqb;1, new edition&rsqb;"
   end
 
   it "formats a single citation with both a prefix and a suffix" do
     expect(formatted_citation("citet:[Erdos65, prefix=see, suffix=new edition]", options: options)).
-      to eq "Erdős et al. [see 1, new edition&rsqb;"
+      to eq "Erdős et al. &lsqb;see 1, new edition&rsqb;"
   end
 
   it "formats a single citation with a standard locator" do
     expect(formatted_citation("citet:[Erdos65, page=41-43]", options: options)).
-      to eq "Erdős et al. [1, pp. 41-43&rsqb;"
+      to eq "Erdős et al. &lsqb;1, pp. 41-43&rsqb;"
   end
 
   it "formats a single citation with a custom locator" do
     expect(formatted_citation("citet:[Erdos65, locator=somewhere]", options: options)).
-      to eq "Erdős et al. [1, somewhere&rsqb;"
+      to eq "Erdős et al. &lsqb;1, somewhere&rsqb;"
   end
 end
